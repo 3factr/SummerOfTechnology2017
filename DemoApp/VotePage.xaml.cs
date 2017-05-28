6f0429b56138cb4975af8ab1950805d50e680cc3
Samuel Debruyn <samuel.debruyn@3factr.be>
2017-05-28 22:47:42 +0200

    use voteservice to vote

--- DemoApp/VotePage.xaml.cs
+++ DemoApp/VotePage.xaml.cs
@@ -5,12 +5,15 @@ namespace DemoApp
 {
     public partial class VotePage : ContentPage
     {
+        readonly VoteService _voteService;
         public ObservableCollection<string> VoteOptions { get; }
 
         public VotePage()
         {
             VoteOptions = new ObservableCollection<string>{"Pizza", "Frietjes", "Kebab", "Pita", "Broodje gezond"};
             BindingContext = this;
+
+            _voteService = new VoteService();
 
             InitializeComponent();
         }
@@ -27,9 +30,16 @@ namespace DemoApp
             var listView = (ListView)sender;
             listView.SelectedItem = null;
 
-            await DisplayAlert("Gestemd!", $"We hebben je stem voor {selected} goed ontvangen.", "OK");
-
-            await Navigation.PopAsync(true);
+            var voteOk = await _voteService.Vote(selected);
+            if(voteOk)
+            {
+                await DisplayAlert("Gestemd!", $"We hebben je stem voor {selected} goed ontvangen.", "OK");
+                await Navigation.PopAsync(true);
+            }
+            else
+            {
+                await DisplayAlert("Mislukt :(", $"We hebben je stem voor {selected} niet goed ontvangen. Probeer later opnieuw.", "OK");
+            }
         }
     }
 }
